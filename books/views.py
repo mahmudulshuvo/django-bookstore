144baffecfcb16c6d6dd7341873c02d921c73f7b
Shuvo hassan <shuvo0201@gmail.com>
2021-07-13 14:01:21 +0600

    Add dynamic routing

--- books/views.py
+++ books/views.py
@@ -8,3 +8,10 @@ data = json.loads(books)
 
 def index(request):
     return render(request, 'books/index.html', {'books': data})
+
+def show(request, id):
+    showBook = {}
+    for book in data:
+        if book['id'] == id:
+            showBook = book
+    return render(request, 'books/show.html', {'book': showBook})
